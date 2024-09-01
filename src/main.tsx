312972d71a120281a8665d82faba6db51c4cd245
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2024-09-01 10:33:33 +0530

    removed secret

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,10 @@ import App from "./App.tsx";
 import "./index.css";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { config } from "./utils/secretConfig.json";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-	apiKey: "AIzaSyCwPizC1XAL5QjKWSiIM-BbLd2Cgrf58vw",
-	authDomain: "webchatapp-devchat.firebaseapp.com",
-	projectId: "webchatapp-devchat",
-	storageBucket: "webchatapp-devchat.appspot.com",
-	messagingSenderId: "120263248466",
-	appId: "1:120263248466:web:f5c653d5ae155da20a6d8b",
-};
+const firebaseConfig = config;
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
