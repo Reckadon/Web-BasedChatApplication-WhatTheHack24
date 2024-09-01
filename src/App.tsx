8045eed03143b4018b208a806954ff94b16ced1a
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2024-09-01 10:38:13 +0530

    removed manual updating of isLoggedIn

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Login from "./components/Login";
 import ChatPage from "./components/ChatPage/ChatPage";
 import "./App.css";
@@ -8,12 +8,16 @@ function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 	const [user, setUser] = useState<User | null>(null);
 
+	useEffect(() => {
+		if (user) setIsLoggedIn(true);
+		else setIsLoggedIn(false);
+	}, [user]);
+
 	const handleLogin = (user: User) => {
-		setIsLoggedIn(true);
 		setUser(user);
 	};
 
-	return <>{isLoggedIn ? <ChatPage user={user} /> : <Login onLogin={handleLogin} />}</>;
+	return <>{isLoggedIn ? <ChatPage user={user as User} /> : <Login onLogin={handleLogin} />}</>;
 }
 
 export default App;
